89499bf994f501f424d7a9fc504c53f16f6383b9
Prashant8722 <prashant2987226@gmail.comPrashant8722>
2023-01-05 09:42:15 +0530

    55- JS Hoisting, Execution Context and Call Stack

--- functionInsideFunction.js
+++ functionInsideFunction.js
@@ -1,23 +1,24 @@
-let a= 10;
-var b=80;
-const c =90;
+// let a1= 10;
+// var b1=80;
+// const c =90;
 
-function hello()
-{
-    var d = 432;
-    var x= 433;
-    function insideHello()
-    {
-        let x = 32;
-        console.log('Insidehello() function x',x);
-    }
-    console.log('hello() function a',a);
-    console.log('hello() function b',b);
-    console.log('hello() function c' ,c);
-    console.log('hello() function d',d);
-    console.log('hello() function x',x);
-}
-console.log('In Global Execution context(GEC) x',x);
-hello();
-insideHello();
-console.log('In Global Execution context(GEC) c' ,c);
+// function hello()
+// {
+//     var d = 432;
+//     var x= 433;
+//     function insideHello()
+//     {
+//         let x = 32;
+//         console.log('Insidehello() function x',x);
+//     }
+//     insideHello();
+//     console.log('hello() function a1',a1);
+//     console.log('hello() function b',b1);
+//     console.log('hello() function c' ,c);
+//     console.log('hello() function d',d);
+//     console.log('hello() function x',x);
+// }
+// // console.log('In Global Execution context(GEC) x',x);  //Error:x is not defined
+// hello();
+// // insideHello(); 
+// console.log('In Global Execution context(GEC) c' ,c);
---
+++ varletconst.js
@@ -0,0 +1,9 @@
+// let ,const --> block Scope
+// var functional scope
+
+// let a=10;
+// {
+//     var a= 100;
+//     console.log('a=',a);
+// }
+
